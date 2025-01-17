aab4b4f65ee58a30c2c347cfc4ae6aae5ec840d6
JhosenthG <jhosenth@gmail.com>
2025-01-17 13:37:41 -0400

    chance screen projects

--- app/src/main/java/com/example/dicerollercodelab/MainActivity.kt
+++ app/src/main/java/com/example/dicerollercodelab/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicerollercodelab.ui.theme.DiceRollerCodeLabTheme
 
 class MainActivity : ComponentActivity() {
@@ -42,5 +50,15 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     Column(
         modifier = Modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {}
+    ) {
+        Image(
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "dice_1"
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { /*TODO*/ })  {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
